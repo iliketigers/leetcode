b4a6cd451361cf4fc77b53d850aec242411d94a3
R L <rpnospam@gmail.com>
2020-06-17 01:43:26 -0700

    pretty sure this works but it own't submit

--- score-of-parentheses.js
+++ score-of-parentheses.js
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 // https://leetcode.com/problems/score-of-parentheses/
 // Given a balanced parentheses string S, compute the score of the string based on the following rule:
-
 // () has score 1
 // AB has score A + B, where A and B are balanced parentheses strings.
 // (A) has score 2 * A, where A is a balanced parentheses string.
@@ -11,7 +10,7 @@
 // const S = "()()" // 2
 // const S = "(()(()))"; // 6
 // const S = "((()))"; // 4
-const S = "(()())()((()))"; // 9
+// const S = "(()())()((()))"; // 9
 
 let previous = null;
 let arr;
@@ -23,6 +22,7 @@ let equation = '';
  */
 const scoreOfParentheses = function (S) {
   arr = S.split('');
+  
   if (arr[0] === '(') {
     if (!previous) {
       equation += '(';
@@ -47,10 +47,9 @@ const scoreOfParentheses = function (S) {
   if (S) {
     equation = scoreOfParentheses(S);
   } else {
-    return equation;
+    return eval(equation);
   }
-  return equation;
+  return eval(equation);
 };
 
-console.log('sup',eval(scoreOfParentheses(S)));
-console.log(equation);
+console.log(eval(scoreOfParentheses(S)));
