8246bb128928be8c61b75f5d361ce89ebbe6c297
R L <rpnospam@gmail.com>
2020-08-08 17:12:17 -0700

    * final solution for 007-palindrome * Runtime: 216 ms, faster than 72.23% of JavaScript online submissions for Palindrome Number. * cleanup for other files.

--- easy_007-reverse-integer.js
+++ easy_007-reverse-integer.js
@@ -6,32 +6,18 @@
  * @return {number}
  */
 const reverse = (x) => {
-
   let reversed = Math.abs(x).toString().split('').reverse().join('');
-  const max = Math.pow(2, 31);
 
-  if (Math.abs(reversed) > max) {
+  if (Math.abs(reversed) > Math.pow(2, 31)) {
     return 0;
   } else {
-
     if (Math.sign(x) === -1) {
-      return -1* reversed;
+      return -1 * reversed;
     } else {
       return Math.abs(reversed);
     }
   }
 };
-
-// const reverse = function (x) {
-//   let reversed = Math.abs(x).toString().split('').reverse().join('');
-//   reversed *= Math.sign(x)
-
-//   if (Math.abs(reversed) > Math.pow(2, 31)) {
-//     return 0;
-//   } else {
-//     return reversed;
-//   }
-// };
 
 console.log(reverse(123)); // 321
 console.log(reverse(9000000)); // 9
--- notes.md
+++ notes.md
@@ -5,3 +5,10 @@
   * two strings mapped to the same number causes a collision
   * hash function runs the same function against every input, and outputs a hash value, which is stored in a set bucket.
   * buckets are pre-determined
+
+# launch.json
+* need to add `"outputCapture" : "std"` to get the debug to work right
+
+# optimizations
+* `foreach` can't be broken; need to do a regular `for` or `while` loop or else it will return `undefined`
+* if want to return `true` or `false`, just return the expression
