9a5f4d0dfe35114096735c32b06f964389985f5c
R L <rpnospam@gmail.com>
2020-08-09 16:11:57 -0700

    working solution

---
+++ easy_014_longest-common-prefix.js
@@ -0,0 +1,31 @@
+// // https://leetcode.com/problems/longest-common-prefix/
+// // Write a function to find the longest common prefix string amongst an array of strings.
+// // If there is no common prefix, return an empty string "".
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  const firstWord = strs[0];
+  if (!firstWord) { return ''; } else {
+    for (i = 0; i < firstWord.length; i++) { // loops only as long as the first word is long
+      let firstWordChar = firstWord[i];
+      for (j = 1; j < strs.length; j++) { // loops only as long as the number of words
+        let word = strs[j];
+        let wordChar = word[i];
+        let reachedEndOfWord = i === word.length;
+        let charsMatch = wordChar === firstWordChar;
+        if (reachedEndOfWord || !charsMatch) { // if we reach the end of the first word, or the chars are different
+          let result = firstWord.substring(0, i);
+          return result; // then get a substring at this point
+        }
+      }
+    }
+    return firstWord; // if blank
+  }
+};
+
+// const array1 = ["fly", "flo", "flower"];
+const array1 = [''];
+console.log(longestCommonPrefix(array1));
