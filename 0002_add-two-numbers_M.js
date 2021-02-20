496244e142600a3bd21a839b8f7ca052e1773385
ria-l <55597349+ria-l@users.noreply.github.com>
2021-02-20 12:02:03 -0800

    cleanup

--- 0002_add-two-numbers_M.js
+++ 0002_add-two-numbers_M.js
@@ -1,36 +1,38 @@
-/* eslint-disable require-jsdoc */
 // https://leetcode.com/problems/add-two-numbers/
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-// Definition for singly-linked list.
-function ListNode(val, next) {
-  this.val = (val === undefined ? 0 : val);
-  this.next = (next === undefined ? null : next);
-}
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 
-// expected: 708
-const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-
-const addTwoNumbers = function (l1, l2) {
+const addTwoNumbers = (l1, l2) => {
   var List = new ListNode(0);
   var head = List;
   var sum = 0;
   var carry = 0;
 
-  while (l1 !== null || l2 !== null || sum > 0) {
-    console.log('list',List);
-    console.log('head',head);
-    if (l1 !== null) {
-      sum = sum + l1.val;
+  while (l1 || l2 || sum > 0) {
+    if (l1) {
+      sum += l1.val;
       l1 = l1.next;
     }
-    if (l2 !== null) {
-      sum = sum + l2.val;
+    if (l2) {
+      sum += l2.val;
       l2 = l2.next;
     }
     if (sum >= 10) {
       carry = 1;
-      sum = sum - 10;
+      sum -= 10;
     }
 
     head.next = new ListNode(sum);
@@ -38,49 +40,6 @@ const addTwoNumbers = function (l1, l2) {
 
     sum = carry;
     carry = 0;
-
   }
-
   return List.next;
-  // let l1Arr = [];
-  // let l2Arr = [];
-  // while (l1 !== null || l2 !== null) {
-  //   l1Arr.push(l1.val);
-  //   l1 = l1.next;
-  //   l2Arr.push(l2.val);
-  //   l2 = l2.next;
-
-  // }
-  // l1Arr = parseInt(l1Arr.reverse().toString().split(',').join(''));
-  // l2Arr = parseInt(l2Arr.reverse().toString().split(',').join(''));
-  //   console.log(l1Arr);
-  //   console.log(l2Arr);
-
-  //   // if list.next = null,
-  //     // ListNode.next = 7
-  //     // else temp list = 
-
-  //   // let sum = l1Arr + l2Arr;
-  //   // let finalList = new ListNode();
-  //   // let lastDigit = sum%10;
-  //   // finalList.val = lastDigit;
-  //   // sum -=lastDigit;
-  //   // sum /=10;
-  //   // console.log(sum);
-  //   // console.log(finalList);
-  //   // finalList.val = lastDigit;
-  //   // console.log(finalList);
-  //   // return sum;
-  // // for the length of the sum, 
-  // let testList = new ListNode();
-  // console.log(testList);
-  // testList.next = new ListNode(3);
-  // console.log(testList);
-  // testList = testList.next;
-  // console.log(testList);
-
-
-
-
 };
-addTwoNumbers(l1, l2);
